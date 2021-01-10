6bee155fa8089a47be2d749b8c400ac98ddd60a0
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2021-01-10 17:46:03 -0500

    Adds 'List with featured podcast' layout

---
+++ shoutem.podcast/app/extension.js
@@ -0,0 +1,20 @@
+// This file is managed by Shoutem CLI
+// It exports screens and themes from extension.json
+// You should not change it manually
+
+// screens imports
+import EpisodesListScreen from './screens/EpisodesListScreen';
+import EpisodesFeaturedListScreen from './screens/EpisodesFeaturedListScreen';
+import EpisodesGridScreen from './screens/EpisodesGridScreen';
+import EpisodesFeaturedGridScreen from './screens/EpisodesFeaturedGridScreen';
+import EpisodeDetailsScreen from './screens/EpisodeDetailsScreen';
+import FeaturedPodcastView from './screens/FeaturedPodcastView';
+
+export const screens = {
+  EpisodesListScreen,
+  EpisodesFeaturedListScreen,
+  EpisodesGridScreen,
+  EpisodesFeaturedGridScreen,
+  EpisodeDetailsScreen,
+  FeaturedPodcastView
+};
